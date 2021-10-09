dad77395a855217cd0841b3e14ecae2d594836c5
韦智凯 <neepoowzk@gmail.com>
2021-10-09 15:27:51 +0800

    refactor: 💡 重构合并集合

--- basic/day05/并查集/acwing_837/main.go
+++ basic/day05/并查集/acwing_837/main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// https://www.acwing.com/problem/content/839/
+
+const N = 100010
+
+var (
+	n, m    int
+	p, size [N]int
+)
+
+func find(x int) int {
+	if p[x] != x {
+		p[x] = find(p[x])
+	}
+	return p[x]
+}
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	reader = bufio.NewReaderSize(reader, 6*N)
+	fmt.Fscanf(reader, "%d %d\n", &n, &m)
+	for i := 1; i <= n; i++ {
+		p[i] = i
+		size[i] = 1
+	}
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() && m > 0 {
+		line := scanner.Text()
+		tokens := strings.Split(line, " ")
+		op := tokens[0]
+
+		switch op {
+		case "C":
+			a, _ := strconv.Atoi(tokens[1])
+			b, _ := strconv.Atoi(tokens[2])
+			if find(a) == find(b) {
+				continue
+			}
+			size[find(b)] += size[find(a)]
+			p[find(a)] = find(b)
+		case "Q1":
+			a, _ := strconv.Atoi(tokens[1])
+			b, _ := strconv.Atoi(tokens[2])
+			if find(a) == find(b) {
+				fmt.Println("Yes")
+			} else {
+				fmt.Println("No")
+			}
+		case "Q2":
+			a, _ := strconv.Atoi(tokens[1])
+			fmt.Println(size[find(a)])
+		}
+		m--
+	}
 }
--- basic/day05/并查集/main.go
+++ basic/day05/并查集/main.go
@@ -1,5 +1,61 @@
+/*
+ 应用
+	1. 合并两个集合
+	2. 询问两个元素是否属于同一个集合
+每个集合用一颗树来维护,每个集合的根节点的编号就是当前集合 bianhao
+
+q1: is root? x?= p(x)
+q2: find root : while(p(x)!=x) x= p[x]
+q3: how merge two tree?
+*/
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const N = 100010
+
+var (
+	n, m int
+	p    [N]int
+)
+
 func main() {
-	$END$
+	reader := bufio.NewReader(os.Stdin)
+	reader = bufio.NewReaderSize(reader, 5*N)
+	fmt.Fscanf(reader, "%d %d\n", &n, &m)
+	for i := 1; i <= n; i++ {
+		p[i] = i
+	}
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() && m > 0 {
+		line := scanner.Text()
+		tokens := strings.Split(line, " ")
+		op := tokens[0]
+		a, _ := strconv.Atoi(tokens[1])
+		b, _ := strconv.Atoi(tokens[2])
+		switch op {
+		case "M":
+			p[find(a)] = find(b)
+		case "Q":
+			if find(a) == find(b) {
+				fmt.Println("Yes")
+			} else {
+				fmt.Println("No")
+			}
+		}
+		m--
+	}
+}
+
+func find(x int) int {
+	if p[x] != x {
+		p[x] = find(p[x])
+	}
+	return p[x]
 }

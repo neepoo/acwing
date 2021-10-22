9f36cbbeb55ef04730eb42b57ecf24262e149153
韦智凯 <neepoowzk@gmail.com>
2021-10-22 19:45:25 +0800

    feat: 🎸 完成leetcode69

---
+++ leetcode/69/solution.go
@@ -0,0 +1,24 @@
+package solution
+
+func mySqrt(x int) int {
+	if x < 1 {
+		return 0
+	}
+	if x == 1{
+		return 1
+	}
+	l, r := 0, x
+	var mid int
+	for k := 0; k < 100; k++ {
+		mid = (l + r) / 2
+		i := mid * mid
+		if i > x {
+			r = mid
+		} else if i < x {
+			l = mid
+		} else {
+			return mid
+		}
+	}
+	return mid
+}

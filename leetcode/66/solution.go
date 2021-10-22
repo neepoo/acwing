2949eb5f733ce72a9fd4199e43a00acf84fc7cf7
韦智凯 <neepoowzk@gmail.com>
2021-10-22 19:08:17 +0800

    feat: 🎸 完成leetcode66

--- leetcode/66/solution.go
+++ leetcode/66/solution.go
@@ -1,1 +1,22 @@
-package _6
+package solution
+
+func plusOne(digits []int) []int {
+	c := 0
+	res := make([]int, len(digits)+1)
+	tmp := -1
+	for i := len(digits) - 1; i >= 0; i-- {
+		if i == len(digits)-1 {
+			tmp = digits[i] + c + 1
+		} else {
+			tmp = digits[i] + c
+		}
+		c = tmp / 10
+		res[i+1] = tmp % 10
+	}
+	if c == 1 {
+		res[0] = 1
+		return res
+	} else {
+		return res[1:]
+	}
+}

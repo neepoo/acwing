f7434d1850ba34ef94fbaacf64788749ccf99b42
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:08:59 +0800

    refactor: 💡 从后往前遍历解决leetcode58
    https://leetcode-cn.com/problems/length-of-last-word/

--- leetcode/58/solution.go
+++ leetcode/58/solution.go
@@ -7,18 +7,32 @@ func lengthOfLastWord(s string) int {
 	st := -1
 	res := -1
 	for i := 0; i < len(s); i++ {
-		for i < len(s) && s[i] == ' '{
+		for i < len(s) && s[i] == ' ' {
 			i++
 		}
 		// 找到非空格开头
 		st = i
-		for i < len(s) && s[i]!=' '{
+		for i < len(s) && s[i] != ' ' {
 			i++
 		}
 		res = i - st
-		if i >= len(s) - 1{
+		if i >= len(s)-1 {
 			break
 		}
 	}
 	return res
+}
+
+func lengthOfLastWordNew(s string) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		for s[i] == ' ' {
+			i--
+		}
+		j := i
+		for j >= 0 && s[j] != ' ' {
+			j--
+		}
+		return i - j
+	}
+	return 0
 }
--- leetcode/58/solution_test.go
+++ leetcode/58/solution_test.go
@@ -2,6 +2,41 @@ package solution
 
 import "testing"
 
+func Test_lengthOfLastWordNew(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{s: "Hello World"},
+			want: 5,
+		},
+		{
+			name: "case2",
+			args: args{s: "   fly me   to   the moon  "},
+			want: 4,
+		},
+		{
+			name: "case3",
+			args: args{s: "luffy is still joyboy"},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWordNew(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+
 func Test_lengthOfLastWord(t *testing.T) {
 	type args struct {
 		s string

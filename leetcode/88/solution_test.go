f6664f1f15e216091e0508b28f023f1d214354df
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:46:32 +0800

    feat: 🎸 leetcode:88
    睁着插入会导致乱序,因此从后往前插入

---
+++ leetcode/88/solution_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		nums1 []int
+		m     int
+		nums2 []int
+		n     int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Case1",
+			args: args{
+				nums1: []int{1, 2, 3, 0, 0, 0},
+				m:     3,
+				nums2: []int{2, 5, 6},
+				n:     3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			merge(tt.args.nums1, tt.args.m, tt.args.nums2, tt.args.n)
+			require.Equal(t, tt.args.nums1, []int{1, 2, 2, 3, 5, 6})
+		})
+	}
+}

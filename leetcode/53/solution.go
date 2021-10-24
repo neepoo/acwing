7499bc601267efeeb30570cc9aafb7ed82b8de65
韦智凯 <neepoowzk@gmail.com>
2021-10-24 12:42:35 +0800

    feat: 🎸 完成leetcode53

--- leetcode/53/solution.go
+++ leetcode/53/solution.go
@@ -1,1 +1,17 @@
-package _3
+package leetcode
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxSubArray(nums []int) int {
+	last, res := -0x3f3f3f3f, -0x3f3f3f3f
+	for i := 0; i < len(nums); i++ {
+		last = max(0, last) + nums[i]
+		res = max(res, last)
+	}
+	return res
+}

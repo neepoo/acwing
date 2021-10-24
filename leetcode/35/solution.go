bb9265c6763b189f403d19e3021e851956afed65
韦智凯 <neepoowzk@gmail.com>
2021-10-24 12:55:57 +0800

    refactor: 💡 new solution
    https://leetcode-cn.com/problems/search-insert-position/submissions/

--- leetcode/35/solution.go
+++ leetcode/35/solution.go
@@ -4,22 +4,37 @@ func searchInsert(nums []int, target int) int {
 	l := 0
 	r := len(nums) - 1
 	mid := (l + r + 1) / 2
-	for l < r{
+	for l < r {
 		mid = (l + r + 1) / 2
 		if nums[mid] <= target {
 			l = mid
-		}else {
+		} else {
 			r = mid - 1
 		}
 	}
 	//log.Printf("l=%d, r = %d, mid=%d\n", l, r, mid)
 	if nums[l] == target {
 		return l
-	}else if nums[l] < target{
-		return l+1
+	} else if nums[l] < target {
+		return l + 1
 	}
-	if l == 0{
+	if l == 0 {
 		return 0
 	}
 	return l - 1
+}
+
+// 从左往右,找到第一个大于等于target的数
+func searchInsertNew(nums []int, target int) int {
+	l := 0
+	r := len(nums)
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] >= target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
 }

81abcb300c576eea3e703a485e86701020a3d63b
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:22:35 +0800

    refactor: 💡 leetcode67:继续优化代码,少写一个if判断
      https://leetcode-cn.com/problems/add-binary

--- leetcode/67/solution.go
+++ leetcode/67/solution.go
@@ -88,3 +88,35 @@ func addBinaryNew(a string, b string) string {
 	}
 	return res
 }
+
+func addBinaryNew1(a string, b string) string {
+	var reverseRes string
+	c := 0
+	var i int
+	for i = 0; i < len(a) || i < len(b) || c > 0; i++ {
+		curAInt := 0
+		curBInt := 0
+		if len(a)-i-1 >= 0 {
+			curA := a[len(a)-i-1]
+			if curA == '1' {
+				curAInt = 1
+			}
+
+		}
+		if len(b)-i-1 >= 0 {
+			curB := b[len(b)-i-1]
+			if curB == '1' {
+				curBInt = 1
+			}
+		}
+		curAns := curAInt + curBInt + c
+		c = curAns / 2
+		reverseRes += strconv.Itoa(curAns % 2)
+
+	}
+	res := ""
+	for k := len(reverseRes) - 1; k >= 0; k-- {
+		res += string(reverseRes[k])
+	}
+	return res
+}

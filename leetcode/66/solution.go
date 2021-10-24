28f6f4a4b33b66d2d638a614087e4959fb9cd338
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:14:09 +0800

    refactor: 💡 leetcode66: 用初始化进位是1,来模拟加1的guocheng
    https://leetcode-cn.com/problems/plus-one/

--- leetcode/66/solution.go
+++ leetcode/66/solution.go
@@ -20,3 +20,20 @@ func plusOne(digits []int) []int {
 		return res[1:]
 	}
 }
+
+func plusOneNew(digits []int) []int {
+	c := 1
+	res := make([]int, len(digits)+1)
+	tmp := -1
+	for i := len(digits) - 1; i >= 0; i-- {
+		tmp = digits[i] + c
+		c = tmp / 10
+		res[i+1] = tmp % 10
+	}
+	if c == 1 {
+		res[0] = 1
+		return res
+	} else {
+		return res[1:]
+	}
+}
--- leetcode/66/solution_test.go
+++ leetcode/66/solution_test.go
@@ -43,3 +43,43 @@ func Test_plusOne(t *testing.T) {
 		})
 	}
 }
+
+
+func Test_plusOneNew(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "case1",
+			args: args{digits: []int{1, 2, 3}},
+			want: []int{1, 2, 4},
+		},
+		{
+			name: "case2",
+			args: args{digits: []int{4, 3, 2, 1}},
+			want: []int{4, 3, 2, 2},
+		},
+		{
+			name: "case3",
+			args: args{digits: []int{0}},
+			want: []int{1},
+		},
+		{
+			name: "case4",
+			args: args{digits: []int{9}},
+			want: []int{1, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOneNew(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

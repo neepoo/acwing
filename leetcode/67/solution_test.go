207471114049f1bae159fbd8cca78d8f5ca7fa9a
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:20:28 +0800

    feat: 🎸 leetcode67:用一个循环代替三个循环,在循环中判断下标是否合法
    https://leetcode-cn.com/problems/add-binary

--- leetcode/67/solution_test.go
+++ leetcode/67/solution_test.go
@@ -37,3 +37,39 @@ func Test_addBinary(t *testing.T) {
 		})
 	}
 }
+
+func Test_addBinaryNew(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "case1",
+			args: args{
+				a: "11",
+				b: "1",
+			},
+			want: "100",
+		},
+		{
+			name: "case2",
+			args: args{
+				a: "1010",
+				b: "1011",
+			},
+			want: "10101",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addBinaryNew(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("addBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

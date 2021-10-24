d2341e7dbedf35bb07f6607e17fc5b80fc57ce71
韦智凯 <neepoowzk@gmail.com>
2021-10-24 13:51:10 +0800

    fix: 🐛 fix github action fail

--- basic/day07/quiz/847/main.go
+++ basic/day07/quiz/847/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+const N = 3
+
+var (
+	p      [N][N]string
+	ix, iy int
+	dx     = [4]int{-1, 0, 1, 0}
+	dy     = [4]int{0, 1, 0, -1}
+)
+
 func main() {
-	$END$
+	var s string
+	for i := 0; i < 9; i++ {
+		fmt.Scanf("%s", &s)
+		p[i/3][i%3] = s
+		if s == "x" {
+			ix = i / 3
+			iy = i % 3
+		}
+	}
+	fmt.Println(p)
 }

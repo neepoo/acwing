f97e42f7f3619c4d5d5e8cf7023e479fa2ba7623
韦智凯 <neepoowzk@gmail.com>
2021-10-25 12:01:03 +0800

    feat: 🎸 完成leetcode101 对称二叉树
    https://leetcode-cn.com/problems/symmetric-tree/

--- leetcode/101/solution.go
+++ leetcode/101/solution.go
@@ -1,1 +1,24 @@
-package _01
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func dfs(q, p *TreeNode) bool {
+	if q == nil && p == nil {
+		return true
+	}
+	if q == nil || p == nil || p.Val != q.Val {
+		return false
+	}
+	return dfs(q.Left, p.Right) && dfs(q.Right, p.Left)
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	return dfs(root.Left, root.Right)
+}

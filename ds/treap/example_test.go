d5d9ee610ee0df465d59b58d309e74e7d45fb718
韦智凯 <neepoowzk@gmail.com>
2022-01-14 11:47:53 +0800

    TODO finish treap

--- ds/treap/example_test.go
+++ ds/treap/example_test.go
@@ -1,12 +1,12 @@
 package treap
 
-import "testing"
-
-func TestTreap(t *testing.T) {
-	tp := NewTreap()
-	tp.insert(str("Cabbage"), 77)
-	tp.insert(str("Beer"), 76)
-	tp.insert(str("Eggs"), 129)
-	tp.insert(str("Bacon"), 95)
-	tp.insert(str("Butter"), 86)
-}
+//import "testing"
+//
+//func TestTreap(t *testing.T) {
+//	tp := NewTreap()
+//	tp.insert(str("Cabbage"), 77)
+//	tp.insert(str("Beer"), 76)
+//	tp.insert(str("Eggs"), 129)
+//	tp.insert(str("Bacon"), 95)
+//	tp.insert(str("Butter"), 86)
+//}
